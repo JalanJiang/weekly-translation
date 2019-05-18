348ac7293550d633e657612b2428301816ac6d07
JalanJiang <448300947@qq.com>
2019-05-18 14:02:31 +0800

    feat(other): The downsides of JSON for config files

--- push.sh
+++ push.sh
@@ -3,5 +3,5 @@ gitbook build
 git add *
 git commit -m "$commit"
 git push origin master
-git subtree pull --prefix=_book origin gh-pages
-git subtree push --prefix=_book origin gh-pages
+# git subtree pull --prefix=_book origin gh-pages
+git subtree push --prefix=_book origin gh-pages --force
